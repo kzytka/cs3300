6b027907e4f5f76c794f0d714cfc6aad8e34717c
Kiley Zytka <kiley.zytka@gmail.com>
2022-03-30 19:43:37 -0600

    Iteration 4 commit update

--- Gemfile
+++ Gemfile
@@ -56,9 +56,6 @@ gem "bootsnap", require: false
 # add mini racer
 gem 'mini_racer'
 
-# add simplecov
-gem 'simplecov', require: false, group: :test
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ProjectsController, type: :controller do
   context "GET #index" do
     it "returns a success response" do
       get :index
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
@@ -12,7 +12,7 @@ RSpec.describe ProjectsController, type: :controller do
     let!(:project) { Project.create(title: "Test title", description: "Test description") }
     it "returns a success response" do
       get :show, params: { id: project }
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 end
